25223ce80fbd1a22774fe90892937a0520b13772
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2022-09-27 07:43:07 +0300

    pdo

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
 <?php
+$dsn = 'mysql:host=127.0.0.1;dbname=studentdb;charset=utf8';
+$user = 'testuser';
+$password = 'qwerty';
+$dbh = new PDO($dsn, $user, $password);
+$sql = "SELECT * FROM students;";
+$stat = $dbh->query($sql);
+$student = $stat->fetch(PDO::FETCH_NAMED);
+$students = $stat->fetchAll();
+//var_dump($students);
+//$students = $stat->fetch();
+//var_dump($students);
+//$stat->closeCursor();
+//$sql = 'INSERT INTO students (name, surname, age, group_id) VALUES ("Ian", "Ivko", 21, 2)';
+//$res = $dbh->exec($sql);
+//$sql = 'INSERT INTO students (name, surname, age, group_id) VALUES (:name, :surname, :age, :group_id)';
+//$req = $dbh->prepare($sql);
+//var_dump($req);
+//$res = $req->execute(
+//        [
+//            "name" => "Юрій",
+//            "surname" => "Андрашко",
+//            "age" => 34,
+//            "group_id" => 1
+//        ]
+//);
+
+//$sql = 'INSERT INTO students (name, surname, age, group_id) VALUES (?, ?, ?, ?)';
+//$req = $dbh->prepare($sql);
+////var_dump($req);
+//$res = $req->execute(
+//    ["Юрій", "Андрашко", "сорок", 2]
+//);
+//var_dump($res);
+//if (!$res)
+//{
+//    var_dump($req->errorInfo());
+//}
     $students = [
         [
             "name" => "Yurii",
@@ -11,22 +48,22 @@
             "surname" => "Andrienko",
         ]
     ];
-echo "<table>";
-echo "<tr> <th> name</th> <th>course</th> </tr>";
-    for ($i=0; $i<count($students); $i++){
-        echo "<tr>";
-            foreach ($students[$i] as $key=>$value){
-                echo "<td>$value</td>";
-            }
-        echo "</tr>";
-    }
-echo "</table>";
-
-//    foreach ($students as $student){
-//        var_dump($student);
+//echo "<table>";
+//echo "<tr> <th> name</th> <th>course</th> </tr>";
+//    for ($i=0; $i<count($students); $i++){
+//        echo "<tr>";
+//            foreach ($students[$i] as $key=>$value){
+//                echo "<td>$value</td>";
+//            }
+//        echo "</tr>";
 //    }
+//echo "</table>";
+//
+////    foreach ($students as $student){
+////        var_dump($student);
+////    }
 ?>
-
+<!---->
 <table>
     <tr>
         <?php foreach (array_keys($students[0]) as $key):?>
@@ -43,3 +80,10 @@ echo "</table>";
     </tr>
     <?php endforeach; ?>
 </table>
+
+
+<form>
+    <input name="surname" value="<?= $student['surname']?>">
+    <input name="name" value="<?= $student['name']?>">
+    <input name="age" type="number" value="<?= $student['age']?>">
+</form>
